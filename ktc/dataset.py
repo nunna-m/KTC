66c6cb92cce4cbc25529083c29ce0f2c35d3b849
nunna-m <maanvi7062@gmail.com>
2021-09-15 16:06:45 +0900

    parse_subject() and depending functions checked and working, resize and crop added. asserting size constraints not done:(

--- ktc/dataset.py
+++ ktc/dataset.py
@@ -53,6 +53,7 @@ def load_raw(traindir, modalities=('am','tm','dc','ec','pc'), output_size=(224,2
     dataset = dataset.interleave(
         partial(
             combine_modalities,
+            output_size=output_size,
             modalities=modalities,
             tumor_region_only=tumor_region_only,
         ),
@@ -71,18 +72,19 @@ def filter_modalities(paths,modalities):
         paths))
     return paths
 
-def combine_modalities(subject, modalities=('am','tm','dc','ec','pc'), tumor_region_only=False):
+def combine_modalities(subject, output_size, modalities=('am','tm','dc','ec','pc'), tumor_region_only=False):
     return tf.py_function(
         lambda x: partial(prep_combined_modalities,
+        output_size,
         modalities=modalities, tumor_region_only=tumor_region_only)(x)['modalities'],
         [subject],
         tf.uint8,
     )
 
-def prep_combined_modalities(subject, modalities, tumor_region_only):
+def prep_combined_modalities(subject, output_size, modalities, tumor_region_only):
     if isinstance(subject, str): pass
     else: raise NotImplementedError
-    subject_data = parse_subject(subject, modalities=modalities, tumor_region_only=tumor_region_only)
+    subject_data = parse_subject(subject, output_size, modalities=modalities, tumor_region_only=tumor_region_only)
     slice_names = subject_data['TRA'].keys()
 
     slices = tf.stack([tf.stack([subject_data[type_][slice_] for type_ in modalities], axis=-1) for slice_ in slice_names])
@@ -94,7 +96,7 @@ def prep_combined_modalities(subject, modalities, tumor_region_only):
         path=subject_data['path'],
     )
 
-def parse_subject(subject_path, modalities,tumor_region_only, decoder=tf.image.decode_image, crop=tf.image.crop_to_bounding_box):
+def parse_subject(subject_path, output_size, modalities,tumor_region_only, decoder=tf.image.decode_image):
     subject_data = {'subject_path': subject_path}
     subject_data['clas'], subject_data['ID'] = get_class_ID_subjectpath(subject_path)
     gathered_modalities_paths = {
@@ -120,22 +122,15 @@ def parse_subject(subject_path, modalities,tumor_region_only, decoder=tf.image.d
     def image_decoder(decode_func):
         def wrapper(img):
             img = tf.io.read_file(img)
-            return decode_func(img)
-        return wrapper
-    decoder = image_decoder(decoder)
-
-    def image_crop(crop_func):
-        def wrapper(img, crop_dict):
-            img = tf.convert_to_tensor(img, dtype=tf.float32)
-            img = crop_func(img, crop_dict['y'], crop_dict['x'], crop_func['height'], crop_dict['width'])
+            img = decode_func(img)
             return img
         return wrapper
-    crop = image_crop(crop)
+    decoder = image_decoder(decoder)
 
     if tumor_region_only:
         for modality, names in gathered_modalities_paths.items():
             subject_data[modality] = {
-                os.path.splitext(name)[0]: crop(decoder(os.path.join(subject_path, modality, name))[:, :, 0] ,get_tumor_boundingbox(os.path.join(subject_path, modality+'L', name))) for name in names
+                os.path.splitext(name)[0]: crop(decoder(os.path.join(subject_path, modality, name))[:, :, 2] ,output_size, get_tumor_boundingbox(os.path.join(subject_path, modality, name),os.path.join(subject_path, modality+'L', name))) for name in names
             } 
     else:
         for modality, names in gathered_modalities_paths.items():
@@ -152,25 +147,42 @@ def get_class_ID_subjectpath(subject):
     assert clas in ('AML', 'CCRCC'), f'Classification category{clas} extracted from : {subject} unknown'
     return clas, ID
 
+def crop(img, resize_shape, crop_dict):
+    
+    tumor_center = {
+            'y':crop_dict['y']+(crop_dict['height']//2),
+            'x':crop_dict['x']+(crop_dict['width']//2),
+    }
+    
+    img = img.numpy()
+    (h, w) = resize_shape    
+    y1 = tumor_center['y'] - h//2
+    x1 = tumor_center['x'] - w//2
+    y2 = tumor_center['y'] + h//2
+    x2 = tumor_center['x'] + w//2
+    for i in [y1,x1,y2,x2]:
+        assert i>0, f'height or width going out of bounds'
+    
+    img = tf.convert_to_tensor(img, dtype=tf.uint8)
+    return img
 
-
-def get_tumor_boundingbox(path):
-    path = '/home/maanvi/LAB/Datasets/kidney_tumor_trainvaltest/am_tm/train/AML/87345564/dcL/2.png'
-    image = cv2.imread(path)[:,:,0]
+def get_tumor_boundingbox(imgpath, labelpath):
+    (orig_height, orig_width) = cv2.imread(imgpath)[:,:,2].shape
+    image = cv2.imread(labelpath)[:,:,2]
+    image = cv2.resize(image, (orig_width, orig_height))
     backup = image.copy()
-    lower_red = np.array([0,0,220])
-    upper_red = np.array([10,10,255])
+    lower_red = np.array([220])
+    upper_red = np.array([255])
     mask = cv2.inRange(image, lower_red, upper_red)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2. CHAIN_APPROX_NONE)
-    for i, contour in enumerate(contours):
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(backup, (x,y), (x+w,y+h),(0,0,255),2)
-    #cv2.imwrite("result.png",backup)
+    c = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(c)
     crop_info = {
         'y': y,
         'x': x,
         'height': h,
         'width': w,
+        'labelpath':labelpath,
     }
     return crop_info
 
--- ktc/temp.py
+++ ktc/temp.py
@@ -6,9 +6,10 @@ import glob
 from functools import partial, wraps
 import cv2
 from tensorflow.python.framework.ops import reset_default_graph
+from tensorflow.python.ops.gen_array_ops import shape
 
 
-def parse_subject(subject_path, modalities,tumor_region_only, decoder=tf.image.decode_image, crop=tf.image.crop_to_bounding_box):
+def parse_subject(subject_path, output_size, modalities,tumor_region_only, decoder=tf.image.decode_image):
     subject_data = {'subject_path': subject_path}
     subject_data['clas'], subject_data['ID'] = get_class_ID_subjectpath(subject_path)
     gathered_modalities_paths = {
@@ -34,22 +35,15 @@ def parse_subject(subject_path, modalities,tumor_region_only, decoder=tf.image.d
     def image_decoder(decode_func):
         def wrapper(img):
             img = tf.io.read_file(img)
-            return decode_func(img)
-        return wrapper
-    decoder = image_decoder(decoder)
-
-    def image_crop(crop_func):
-        def wrapper(img, crop_dict):
-            img = tf.convert_to_tensor(img, dtype=tf.uint8)
-            img = crop_func(img, crop_dict['y'], crop_dict['x'], crop_dict['height'], crop_dict['width'])
+            img = decode_func(img)
             return img
         return wrapper
-    crop = image_crop(crop)
+    decoder = image_decoder(decoder)
 
     if tumor_region_only:
         for modality, names in gathered_modalities_paths.items():
             subject_data[modality] = {
-                os.path.splitext(name)[0]: crop(decoder(os.path.join(subject_path, modality, name))[:, :, 0] ,get_tumor_boundingbox(os.path.join(subject_path, modality+'L', name))) for name in names
+                os.path.splitext(name)[0]: crop(decoder(os.path.join(subject_path, modality, name))[:, :, 2] ,output_size, get_tumor_boundingbox(os.path.join(subject_path, modality, name),os.path.join(subject_path, modality+'L', name))) for name in names
             } 
     else:
         for modality, names in gathered_modalities_paths.items():
@@ -66,27 +60,82 @@ def get_class_ID_subjectpath(subject):
     assert clas in ('AML', 'CCRCC'), f'Classification category{clas} extracted from : {subject} unknown'
     return clas, ID
 
+def crop(img, resize_shape, crop_dict):
+    
+    tumor_center = {
+            'y':crop_dict['y']+(crop_dict['height']//2),
+            'x':crop_dict['x']+(crop_dict['width']//2),
+    }
+    
+    img = img.numpy()
+    (h, w) = resize_shape    
+    y1 = tumor_center['y'] - h//2
+    x1 = tumor_center['x'] - w//2
+    y2 = tumor_center['y'] + h//2
+    x2 = tumor_center['x'] + w//2
+    for i in [y1,x1,y2,x2]:
+        assert i>0, f'height or width going out of bounds'
+    
+    img = tf.convert_to_tensor(img, dtype=tf.uint8)
+    return img
 
-
-def get_tumor_boundingbox(path):
-    image = cv2.imread(path)[:,:,0]
+def get_tumor_boundingbox(imgpath, labelpath):
+    (orig_height, orig_width) = cv2.imread(imgpath)[:,:,2].shape
+    image = cv2.imread(labelpath)[:,:,2]
+    image = cv2.resize(image, (orig_width, orig_height))
     backup = image.copy()
     lower_red = np.array([220])
     upper_red = np.array([255])
     mask = cv2.inRange(image, lower_red, upper_red)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2. CHAIN_APPROX_NONE)
-    for i, contour in enumerate(contours):
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(backup, (x,y), (x+w,y+h),(0,0,255),2)
-    #cv2.imwrite("result.png",backup)
+    c = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(c)
     crop_info = {
         'y': y,
         'x': x,
         'height': h,
         'width': w,
+        'labelpath':labelpath,
     }
     return crop_info
 
-subject_data = parse_subject(subject_path='/home/maanvi/LAB/Datasets/kidney_tumor_trainvaltest/am_tm/train/AML/87345564', modalities=['am','tm'], tumor_region_only=True)
+subject_data = parse_subject(subject_path='/home/maanvi/LAB/Datasets/kidney_tumor_trainvaltest/am_tm/train/AML/87345564', output_size = (224,224), modalities=['am','tm'], tumor_region_only=True)
 
 print(subject_data)
+
+
+# backup = img.numpy()
+# print("backup shape: ",backup.shape)
+# cv2.rectangle(backup, (crop_dict['x'],crop_dict['y']), (crop_dict['x']+crop_dict['width'],crop_dict['y']+crop_dict['height']),(0,0,255),2)
+# # cv2.imwrite('result.png', backup)
+# img = tf.convert_to_tensor(img, dtype=tf.uint8)
+# img = tf.reshape(img,  shape=(1,img.shape[0], img.shape[1],1))
+# NUM_BOXES = 1
+# boxes = tf.constant([crop_dict['y'], crop_dict['x'], crop_dict['y']+crop_dict['height'], crop_dict['x']+crop_dict['width']],shape=(NUM_BOXES,4))
+# boxes = tf.cast(boxes, dtype=tf.float32)
+# box_indices = tf.random.uniform(shape=(NUM_BOXES,), minval=0, maxval=1, dtype=tf.int32)
+# img = crop_func(img, boxes, box_indices, resize_shape, method='nearest')
+# print("image shape before reshape: ",img.shape)
+# img = tf.reshape(img, shape=(img.shape[1], img.shape[2]))
+# cv2.imwrite('result.png', img[0:,:,:,0].numpy())
+
+
+
+#zoom_out = False
+#h, w = img.shape[:2]
+# if zoom_out:
+#     height_add = (h - crop_dict['height'])//10
+#     width_add = (w - crop_dict['width'])//10
+#     y1 = crop_dict['y'] - height_add
+#     x1 = crop_dict['x'] - width_add
+#     y2 = crop_dict['y'] + crop_dict['height'] + height_add
+#     x2 = crop_dict['x'] + crop_dict['width'] + width_add
+# else:
+#     y1 = crop_dict['y']
+#     x1 = crop_dict['x']
+#     y2 = crop_dict['y'] + crop_dict['height']
+#     x2 = crop_dict['x'] + crop_dict['width']
+
+
+#cv2.imwrite(os.path.splitext(crop_dict['labelpath'])[0]+'cropped_resized_zoomout.png', img)
+#cv2.imwrite(os.path.splitext(crop_dict['labelpath'])[0]+'cropped_resized.png', img)
