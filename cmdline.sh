b089a9f852419263b91e106e885087e2cdb8ed84
nunna-m <maanvi7062@gmail.com>
2021-11-16 12:39:04 +0900

    changed bash script

--- cmdline.sh
+++ cmdline.sh
@@ -1,2 +1,2 @@
-python3 -m ktc train --whichos remote --modalities am tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities dc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities dc --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
+python3 -m ktc train --whichos remote --modalities am tm --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
