6b6ab348c5aeeb212292385f5bc9deee60f0c3b7
nunna-m <maanvi7062@gmail.com>
2021-11-18 11:50:59 +0900

    changed bash remote

--- cmdline.sh
+++ cmdline.sh
@@ -1,10 +1,1 @@
 python3 -m ktc train --whichos remote --modalities am --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities dc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities ec --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities dc ec --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities ec pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities dc pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities am dc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities am ec --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
--- cmdline2.sh
+++ cmdline2.sh
@@ -1,3 +1,15 @@
+python3 -m ktc train --whichos remote --modalities am --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities dc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities ec --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities dc ec --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities ec pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities dc pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities am dc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --modalities am ec --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+
+
 python3 -m ktc train --whichos remote --modalities am pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
 python3 -m ktc train --whichos remote --modalities dc tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
 python3 -m ktc train --whichos remote --modalities ec tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
