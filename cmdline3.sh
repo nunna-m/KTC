b6cadd5c8073414f3cd13269438821457f215128
nunna-m <maanvi7062@gmail.com>
2021-11-18 13:00:33 +0900

    changed remote bash script

--- cmdline3.sh
+++ cmdline3.sh
@@ -1,8 +1,7 @@
-python3 -m ktc train --whichos remote --modalities am dc tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities am dc ec pc tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities am ec tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities am pc tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities dc pc tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities ec pc tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities dc ec pc --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
-python3 -m ktc train --whichos remote --modalities am tm --config "configfiles/extra/data_options.yaml" --max_steps 100 --validate
+python3 -m ktc train --whichos remote --fold 1 --network resnet --modalities dc --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
+python3 -m ktc train --whichos remote --fold 1 --network resnet --modalities ec --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
+python3 -m ktc train --whichos remote --fold 1 --network resnet --modalities pc --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
+python3 -m ktc train --whichos remote --fold 1 --network resnet --modalities dc ec pc --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
+python3 -m ktc train --whichos remote --fold 1 --network resnet --modalities am --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
+python3 -m ktc train --whichos remote --fold 1 --network resnet --modalities tm --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
+python3 -m ktc train --whichos remote --fold 1 --network resnet --modalities am tm --config "configfiles/extra/data_options.yaml" --max_steps 50 --validate
