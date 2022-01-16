3a34433893febeedb477d4ad15ea577e586ef887
nunna-m <maanvi7062@gmail.com>
2022-01-17 00:01:33 +0900

    combined train val test subjects tested out in testing.py created 5 folds' files with train and test subject paths and shuffled data

--- ktc/pre/crossval.py
+++ ktc/pre/crossval.py
@@ -3,6 +3,7 @@ import shutil
 import math
 import random
 import yaml
+import glob
 
 from . import trainvaltest
 
@@ -13,3 +14,22 @@ def gen_crossval(kfolds=1):
     '''
     trainvaltest.ppp('Yay')
     return
+
+def combine_train_test_val(path):
+    '''
+    For a given modality combines the subjects of train val test folders and stores in a file
+    Args:
+        path: path of the modality whose children folders are train val test
+    '''
+    paths = {'AML':list(), 'CCRCC':list()}
+    all_classes_path = glob.glob(path+'/*/*/*')
+
+    for subject in all_classes_path:
+        subjectCopy = subject[:]
+        clas = subjectCopy.rsplit(os.path.sep, 2)[-2]
+        paths[clas].append(subject)
+
+    with open(os.path.join(path,'allSubjectPaths.yaml'),'w') as file:
+        yaml.dump(paths,file)
+    
+             
---
+++ ktc/testing.py
@@ -0,0 +1,56 @@
+import os
+import shutil 
+import math
+import random
+import yaml
+import glob
+from sklearn.model_selection import KFold
+import numpy as np
+
+path = r'D:\\01_Maanvi\\LABB\\datasets\\kt_new_trainvaltest\\fold1\\am_dc_ec_pc_tm'
+paths = {'AML':list(), 'CCRCC':list()}
+all_classes_path = glob.glob(path+'/*/*/*')
+#print(all_classes_path)
+
+for subject in all_classes_path:
+    subjectCopy = subject[:]
+    clas = subjectCopy.rsplit(os.path.sep, 2)[-2]
+    paths[clas].append(subject)
+
+# with open(os.path.join(path,'allSubjectPaths.yaml'),'w') as file:
+#     yaml.dump(paths,file)
+
+clas_data = dict()
+for clas in paths:
+   # print(clas, len(paths[clas]))
+    clas_data[clas] = {'train_len':int(0.7*len(paths[clas]))}
+    clas_data[clas]['test_len']=len(paths[clas]) - clas_data[clas]['train_len']
+    clas_data[clas]['num'] = len(paths[clas])
+
+k5 = KFold(n_splits=5, shuffle=False)
+
+
+train_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
+test_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
+for clas in ['AML', 'CCRCC']:
+    fold_num = 0
+    for train_index, test_index in k5.split(paths[clas]):
+        print("Fold:{}".format(fold_num))
+        print(train_index, test_index)
+        train_full[fold_num].extend(np.take(paths[clas],train_index))
+        test_full[fold_num].extend(np.take(paths[clas],test_index))
+        fold_num+=1
+
+for i in range(5):
+    random.shuffle(train_full[i])
+    random.shuffle(test_full[i])
+    new_train_full = [str(pat) for pat in train_full[i]]
+    new_test_full = [str(pat) for pat in test_full[i]]
+    store = {'train':new_train_full,
+            'test':new_test_full}
+    with open(os.path.join(path,'allSubjectPaths{}.yaml'.format(i)),'w') as file:
+        yaml.dump(store,file)
+
+    
+
+#print(clas_data)
