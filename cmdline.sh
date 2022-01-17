38defe690d828fc3a78805149e8e1dfbccce5e18
nunna-m <maanvi7062@gmail.com>
2022-01-17 21:00:16 +0900

    added cmdline arg to input ct or mri method

--- cmdline.sh
+++ cmdline.sh
@@ -1,4 +1,3 @@
 python3 -m ktc train_stacked --whichos remote --method CT --config "configfiles/extra/stacked_data_options.yaml" --max_steps 100
-python3 -m ktc train_stacked --whichos remote --method MRI --config "configfiles/extra/stacked_data_options.yaml" --max_steps 100
 
 #python -m ktc train_stacked --whichos windows --method MRI --config "configfiles/extra/stacked_data_options.yaml" --max_steps 3
