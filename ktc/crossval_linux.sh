412fd305eb3c6445cdd2bdda27d25036da0b8f95
nunna-m <maanvi7062@gmail.com>
2022-04-06 17:21:32 +0900

    added k fold CV and Leave one out CV

--- ktc/crossval_linux.sh
+++ ktc/crossval_linux.sh
@@ -25,3 +25,31 @@ python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalitie
 python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am tm --kfolds 5
 python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc ec pc --kfolds 5
 python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am dc ec pc tm --kfolds 5
+
+
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities ec --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities pc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc ec --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities ec pc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc pc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am dc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am ec --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am pc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities ec tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities pc tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am dc ec --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am ec pc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am dc pc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc ec tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities ec pc tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc pc tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am dc tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am ec tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am pc tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am tm --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities dc ec pc --kfolds 10
+python -m pre  crossvalDataGen --whichos linux --path pre/paths.yaml --modalities am dc ec pc tm --kfolds 10
--- ktc/pre/crossval.py
+++ ktc/pre/crossval.py
@@ -3,12 +3,12 @@ import os
 import random
 import yaml
 import glob
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, StratifiedKFold, LeavePOut, LeaveOneOut
 import numpy as np
 
 from . import trainvaltest
 
-def crossvalDataGen(whichos, path, modalities, kfolds, holdout):
+def crossvalDataGen(whichos, path, modalities, kfolds):
     '''
     Outer function to call on combining train val and test subjects. 
     Sample_command: python -m pre crossvalDataGen --path /home/user/path_to_config --modalities /list/of/modalities --kfolds number/of/folds/to/split --whichos operating_sys
@@ -17,7 +17,6 @@ def crossvalDataGen(whichos, path, modalities, kfolds, holdout):
         path: path to the config file that has data paths
         modalities (list[str]): pass a list of modalities to generate files for crossval subjects
         kfolds: number of crossvalidation folds
-        holdout (bool): take out test data for holdout yes if included in CMDline else no
 
     '''
     folds = int(kfolds)
@@ -33,41 +32,66 @@ def crossvalDataGen(whichos, path, modalities, kfolds, holdout):
 
 def combine_train_test_val(path, kfolds=1):
     '''
-    For a given modality combines the subjects of train val test folders and stores in a file + generates 5 folds CV data from all subjects and stores 5 files each with train and test subjects
+    For a given modality combines the subjects of train val test folders and stores in a file + generates k folds CV data + stratified k folds CV + leave_one_out CV --from all subjects and stores separate files each with train and test subjects for each fold
     Args:
         path: path of the modality whose children folders are train val test
     '''
     paths = {'AML':list(), 'CCRCC':list()}
     all_classes_path = glob.glob(path+'/*/*/*')
-
+    target_classes = []
     for subject in all_classes_path:
         subjectCopy = subject[:]
         clas = subjectCopy.rsplit(os.path.sep, 2)[-2] #AML or CCRCC
         paths[clas].append(subject)
-
-    #working here !!!!!
+        target_classes.append(clas)
+    
     with open(os.path.join(path,'allSubjectPaths.yaml'),'w') as file:
         yaml.dump(paths,file)
-    k5 = KFold(n_splits=kfolds, shuffle=False)
+
+    #k folds CV
+    k = KFold(n_splits=kfolds, shuffle=False)
     train_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
     test_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
     for clas in ['AML', 'CCRCC']:
         fold_num = 0
-        for train_index, test_index in k5.split(paths[clas]):
+        for train_index, test_index in k.split(paths[clas]):
             print("Fold:{}".format(fold_num))
             print(train_index, test_index)
             train_full[fold_num].extend(np.take(paths[clas],train_index))
             test_full[fold_num].extend(np.take(paths[clas],test_index))
             fold_num+=1
-
-    for i in range(5):
+    storeCVPath = os.path.join(path,'{}CV'.format(kfolds))
+    os.makedirs(storeCVPath, exist_ok=True)
+    for i in range(kfolds):
         random.shuffle(train_full[i])
         random.shuffle(test_full[i])
         new_train_full = [str(pat) for pat in train_full[i]]
         new_test_full = [str(pat) for pat in test_full[i]]
         store = {'train':new_train_full,
                 'test':new_test_full}
-        with open(os.path.join(path,'allSubjectPaths{}.yaml'.format(i)),'w') as file:
+        with open(os.path.join(storeCVPath,'allSubjectPaths{}.yaml'.format(i)),'w') as file:
             yaml.dump(store,file)
     
-             
+    #leave-one-out CV
+    k = LeavePOut(p=2)
+    train_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
+    test_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
+    for clas in ['AML', 'CCRCC']:
+        fold_num = 0
+        for train_index, test_index in k.split(paths[clas]):
+            print("Fold:{}".format(fold_num))
+            print(train_index, test_index)
+            train_full[fold_num].extend(np.take(paths[clas],train_index))
+            test_full[fold_num].extend(np.take(paths[clas],test_index))
+            fold_num+=1
+    storeCVPath = os.path.join(path,'{}Leave2outCV'.format(kfolds))
+    os.makedirs(storeCVPath, exist_ok=True)
+    for i in range(kfolds):
+        random.shuffle(train_full[i])
+        random.shuffle(test_full[i])
+        new_train_full = [str(pat) for pat in train_full[i]]
+        new_test_full = [str(pat) for pat in test_full[i]]
+        store = {'train':new_train_full,
+                'test':new_test_full}
+        with open(os.path.join(storeCVPath,'allSubjectPaths{}.yaml'.format(i)),'w') as file:
+            yaml.dump(store,file)
