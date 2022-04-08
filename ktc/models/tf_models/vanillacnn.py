3ba4c626e16cead812b3e942c3c73f16f1368bd3
nunna-m <maanvi7062@gmail.com>
2022-04-08 16:30:36 +0900

    test out visualization of augmented images and change vanilla CNN architecture (ref paper)

--- ktc/models/tf_models/vanillacnn.py
+++ ktc/models/tf_models/vanillacnn.py
@@ -25,6 +25,55 @@ class CNN(Model):
         trainable=True,
         activation='relu',
         classifier_activation='softmax',
+        num_classes=2,
+        **kargs,
+    ):
+        super().__init__(**kargs)
+         # define all layers in init
+        activation=layers.LeakyReLU()
+        # Layer of Block 1
+        self.conv1 = layers.Conv2D(64, 5, strides=2, activation=activation)
+        self.max1  = layers.MaxPooling2D(3)
+        self.bn1   = layers.BatchNormalization()
+
+        # Layer of Block 2
+        self.conv2 = layers.Conv2D(128, 5, strides=2, activation=activation)
+        self.bn2   = layers.BatchNormalization()
+        self.drop  = layers.Dropout(0.3)
+
+        # Layer of Block 3
+        self.conv3 = layers.Conv2D(256, 3, strides=2, activation=activation)
+        self.bn2   = layers.BatchNormalization()
+        self.drop  = layers.Dropout(0.5)
+
+        self.flatten = layers.Flatten()
+        # GAP, followed by Classifier
+        self.gap   = layers.GlobalAveragePooling2D()
+        self.dense = layers.Dense(64)
+        self.classify = layers.Dense(num_classes, activation=classifier_activation)
+    
+    @tf.function
+    def call(self, input_tensor, training=False):
+        x = self.conv1(input_tensor)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        x = self.drop(x)
+        x = self.classify(x)
+        return x
+    
+
+class oldCNN(Model):
+    def __init__(
+        self,
+        filters=None,
+        kernel_size=None,
+        strides=None,
+        dropout=None,
+        trainable=True,
+        activation='relu',
+        classifier_activation='softmax',
         num_classes=1,
         **kargs,
     ):
--- ktc/testing.py
+++ ktc/testing.py
@@ -4,39 +4,120 @@ import math
 import random
 import yaml
 import glob
-from sklearn.model_selection import KFold
 import numpy as np
+import tensorflow as tf
+import cv2
+import matplotlib.pyplot as plt
 
-path = r'D:\\01_Maanvi\\LABB\\datasets\\kt_new_trainvaltest\\fold1\\am_dc_ec_pc_tm'
-paths = {'AML':list(), 'CCRCC':list()}
-all_classes_path = glob.glob(path+'/*/*/*')
+modality = 'dc_ec_pc'
+num_mods = 3
+folds = 10
+datapath = '/home/maanvi/LAB/Datasets/kt_new_trainvaltest/fold1/dc_ec_pc/10CV/allSubjectPaths0.yaml'
+train_subjects, test_subjects = [], []
+with open(datapath,'r') as file:
+    data = yaml.safe_load(file)
+train_subjects.extend(data['train'])
+test_subjects.extend(data['test'])
+mapping = {'AML': 0, 'CCRCC':1}
 
-for subject in all_classes_path:
-    subjectCopy = subject[:]
-    clas = subjectCopy.rsplit(os.path.sep, 2)[-2]
-    paths[clas].append(subject)
+def read_img(path, file):
+    imgpath = os.path.join(path, file)
+    labelpath = os.path.join(path+'L', file)
+    orig_image = cv2.imread(imgpath)[:,:,0]
+    (orig_height, orig_width) = cv2.imread(imgpath)[:,:,0].shape
+    image = cv2.imread(labelpath)
+    image = cv2.resize(image, (orig_width, orig_height))
+    backup = image.copy()
+    lower_red = np.array([0,0,50])
+    upper_red = np.array([0,0,255])
+    mask = cv2.inRange(image, lower_red, upper_red)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2. CHAIN_APPROX_NONE)
+    c = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(c)
+    assert (w,h)<(224,224)
+    assert (x,y)>=(0,0)
+    const = 0.3
+    diff_x = int(const*w)
+    diff_y = int(const*h)
+    if (x-diff_x)<0:
+        x1 = 0
+    else:
+        x1 = x-diff_x
+    if (y-diff_y)<0:
+        y1 = 0
+    else:
+        y1 = y-diff_y
+    if (x+w+diff_x)>=orig_width:
+        x2 = orig_width
+    else:
+        x2 = x+diff_x+w
+    if (y+diff_y+h)>=orig_height:
+        y2 = orig_height
+    else:
+        y2 = y+diff_y+h
 
-with open(os.path.join(path,'allSubjectPaths.yaml'),'w') as file:
-    yaml.dump(paths,file)
 
-k5 = KFold(n_splits=5, shuffle=False)
-train_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
-test_full = {0:list(),1:list(),2:list(),3:list(),4:list()}
-for clas in ['AML', 'CCRCC']:
-    fold_num = 0
-    for train_index, test_index in k5.split(paths[clas]):
-        print("Fold:{}".format(fold_num))
-        print(train_index, test_index)
-        train_full[fold_num].extend(np.take(paths[clas],train_index))
-        test_full[fold_num].extend(np.take(paths[clas],test_index))
-        fold_num+=1
+    #gaussian standardizes only modality am
+    tmp = imgpath.rsplit(os.path.sep,2)[1]
+    if tmp=='am':
+        mean, std = orig_image.mean(), orig_image.std()
+        orig_image = (orig_image - mean)/std
+        mean, std = orig_image.mean(), orig_image.std()
+        orig_image = np.clip(orig_image, -1.0, 1.0)
+        orig_image = (orig_image + 1.0) / 2.0
+        orig_image *= 255
+    backup = orig_image[y1:y2,x1:x2]
+    backup = cv2.resize(backup, (224,224),interpolation = cv2.INTER_LINEAR)
+    backup = tf.convert_to_tensor(backup, dtype=tf.uint8)
+    return backup
 
-for i in range(5):
-    random.shuffle(train_full[i])
-    random.shuffle(test_full[i])
-    new_train_full = [str(pat) for pat in train_full[i]]
-    new_test_full = [str(pat) for pat in test_full[i]]
-    store = {'train':new_train_full,
-            'test':new_test_full}
-    with open(os.path.join(path,'allSubjectPaths{}.yaml'.format(i)),'w') as file:
-        yaml.dump(store,file)
+
+def gen_data_from_paths(subject_paths):
+    retX = []
+    rety = []
+    for path in subject_paths:
+        parts = path.rsplit(os.path.sep, 4)
+        mods = parts[1].split('_')
+        y = mapping[parts[3]]
+        filenames = [x[0] for x in os.listdir(os.path.join(path, mods[0]))]#filename just number so only 2 if file is 2.png
+        #print("Filenames: ",filenames)
+        X = []
+        XDict = {i: list() for i in filenames}
+        #print(XDict)
+        for mod in mods:
+            newPath = os.path.join(path, mod)
+            for file in os.listdir(newPath):
+                num = file[0]
+                XDict[num].append(read_img(newPath, file))
+        
+        for num in XDict:
+            temp = np.dstack(XDict[num])
+            X.append(temp)
+            #print("num: {} X shape: {} y={}".format(num, temp.shape, y))
+        X = np.stack(X)
+        retX.append(X)
+        rety.extend([y]*len(filenames))
+    return np.vstack(retX), np.vstack(rety)
+
+X_train, y_train = gen_data_from_paths(train_subjects)
+X_test, y_test = gen_data_from_paths(test_subjects)
+print(X_train.shape, y_train.shape)
+
+# (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+# print(X_train.shape, y_train.shape)
+
+#plotting data
+dataset = tf.data.Dataset.from_tensor_slices((X_train[0:2]/255.0).astype(np.float32))
+
+def plot_images(dataset, n_images, samples_per_image):
+    output = np.zeros((8 * n_images, 8 * samples_per_image, num_mods))
+    row = 0
+    for images in dataset.repeat(samples_per_image).batch(n_images):
+        output[:, row*8:(row+1)*8] = np.vstack(images.numpy())
+        row += 1
+    
+    plt.figure()
+    plt.imshow(output)
+    plt.show()
+
+plot_images(dataset, n_images=2, samples_per_image=4)
