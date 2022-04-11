41ca19cf03e102765b1a286f31b5891052236519
nunna-m <maanvi7062@gmail.com>
2022-04-11 16:55:37 +0900

    remove modalities already processed from bash file

--- cmdline1.sh
+++ cmdline1.sh
@@ -1,8 +1,8 @@
-python3 -m ktc train --whichos remote --modalities am --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities tm --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities dc --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities ec --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities pc --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
+# python3 -m ktc train --whichos remote --modalities am --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
+# python3 -m ktc train --whichos remote --modalities tm --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
+# python3 -m ktc train --whichos remote --modalities dc --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
+# python3 -m ktc train --whichos remote --modalities ec --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
+# python3 -m ktc train --whichos remote --modalities pc --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
 python3 -m ktc train --whichos remote --modalities dc ec --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
 python3 -m ktc train --whichos remote --modalities ec pc --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
 python3 -m ktc train --whichos remote --modalities dc pc --method both --network cnn --config "configfiles/extra/data_options.yaml" --max_steps 50
