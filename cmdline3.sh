ec6593d0be918e10a39ca997a16158ff0780f78d
nunna-m <maanvi7062@gmail.com>
2022-04-12 16:39:40 +0900

    change configs for linearcombicnn experiments

--- cmdline3.sh
+++ cmdline3.sh
@@ -1,9 +1,9 @@
-python3 -m ktc train --whichos remote --modalities dc ec tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities ec pc tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities dc pc tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities am dc tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities am ec tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities am pc tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities am tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-python3 -m ktc train --whichos remote --modalities dc ec pc --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
-# python3 -m ktc train --whichos remote --modalities am dc ec pc tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50
+python3 -m ktc train --whichos remote --modalities dc ec tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+python3 -m ktc train --whichos remote --modalities ec pc tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+python3 -m ktc train --whichos remote --modalities dc pc tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+python3 -m ktc train --whichos remote --modalities am dc tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+python3 -m ktc train --whichos remote --modalities am ec tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+python3 -m ktc train --whichos remote --modalities am pc tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+python3 -m ktc train --whichos remote --modalities am tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+python3 -m ktc train --whichos remote --modalities dc ec pc --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
+# python3 -m ktc train --whichos remote --modalities am dc ec pc tm --method both --network linearcombicnn --config "configfiles/extra/data_options.yaml" --max_steps 30
