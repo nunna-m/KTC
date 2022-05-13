91152e12c558b16f600344557859bd7dd30f1486
nunna-m <maanvi7062@gmail.com>
2022-05-13 18:39:46 +0900

    change cmdline parameters for stacked models
    stacked model has level0 and level1 makes use of all 5 modalities

--- bash/cmdline_ct.sh
+++ bash/cmdline_ct.sh
@@ -1,5 +1,11 @@
-python3 -m ktc meta_learner --whichos remote --config "configfiles/extra/stacked_data_options.yaml" --max_steps 30 --level0 cnn --level1 xgb
+python3 -m ktc meta_learner --whichos remote --config "configfiles/extra/stacked_data_options.yaml" --max_steps 30 --level0 cnn --level1 fc --level0_filename stacked_test
 
 #python -m ktc train_stacked --whichos windows --method MRI --config "configfiles/extra/stacked_data_options.yaml" --max_steps 1
 
 #python -m ktc meta_learner --whichos windows --config "configfiles/extra/stacked_data_options.yaml" --max_steps 1 --level0 cnn --level1 fc
+
+# python -m ktc train_stacked --whichos linux --method MRI --network cnn --config "configfiles/extra/stacked_data_options.yaml" --max_steps 1 --filename stacked_test_mri
+
+# python3 -m ktc meta_learner --whichos remote --config "configfiles/extra/stacked_data_options.yaml" --max_steps 1 --level0 cnn --level1 fc
+
+# python3 -m ktc meta_learner --whichos linux --config "configfiles/extra/stacked_data_options.yaml" --max_steps 5 --level0 cnn --level1 fc --level0_filename stacked_test
