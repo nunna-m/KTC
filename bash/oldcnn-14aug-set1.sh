ed4274f4d3de8c6f0eaa0bc5d1cecb06d919b205
nunna-m <maanvi7062@gmail.com>
2022-05-16 13:49:16 +0900

    exact tumor new bash files

---
+++ bash/oldcnn-14aug-set1.sh
@@ -0,0 +1,8 @@
+python3 -m ktc train --whichos remote --modalities am --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities tm --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities dc --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities ec --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities pc --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities dc ec --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities ec pc --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities dc pc --method both --network oldcnn --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename maxPoolCNN_14aug_exactTumor
