2500669452d49e0fa19f173e65ef33e0a911955f
nunna-m <maanvi7062@gmail.com>
2022-05-17 00:13:30 +0900

    add vgg exp bash for exact tumor

---
+++ bash/vgg16-14aug-set1.sh
@@ -0,0 +1,8 @@
+python3 -m ktc train --whichos remote --modalities am --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities tm --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities dc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities ec --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities pc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities dc ec --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities ec pc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
+python3 -m ktc train --whichos remote --modalities dc pc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_14aug_exactTumor
