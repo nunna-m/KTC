488d687e92d3211142a1ec9752b650bd7c9dff5e
nunna-m <maanvi7062@gmail.com>
2022-07-26 19:19:26 +0900

    functions to combine modalities of a subject

--- ktc/modalityStack.py
+++ ktc/modalityStack.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import cv2
+from regex import P
 '''
 subject_data = {
     'subject_path': 'D:\\01_Maanvi\\LABB\\datasets\\kt_new_trainvaltest\\fold1\\am_ec\\train\\AML\\16313384', 'clas': 'AML', \
@@ -151,11 +152,15 @@ def getImage(imagePath, labelPath, cropType=None):
         return get_exact_tumor(imagePath, labelPath)
 
 
-def getSubjectData(subject_path, modalities, clas):
-#subject_path = r'D:\Ddesktop\17235387'
-#subject_path = '/home/maanvi/LAB/Datasets/kt_new_trainvaltest/am_dc_ec_pc_tm/train/AML/89668505'
-#modalities = ['am','dc','ec','pc','tm']
-#clas = 'AML'
+def getSubjectData(subject_path, cropType):
+    print(cropType)
+    pathParts = subject_path.rsplit(os.path.sep, 4)
+    modalities = pathParts[1].split('_')
+    subject_data = {}
+    subject_data['path'] = subject_path
+    subject_data['modalities'] = modalities
+    subject_data['clas'] = str(pathParts[3])
+    subject_data['ID'] = str(pathParts[4])
     gathered_modalityPaths = {
         modality: set(
             os.listdir(
@@ -166,7 +171,6 @@ def getSubjectData(subject_path, modalities, clas):
         )
         for modality in modalities
     }
-    #print(gathered_modalityPaths)
     same_named_imageNames = set.intersection(
         *map(
             lambda slices:
@@ -177,7 +181,6 @@ def getSubjectData(subject_path, modalities, clas):
             ), gathered_modalityPaths.values()
         )
     )
-
     for temp in modalities:
             gathered_modalityPaths[temp] = {k+'.png' for k in same_named_imageNames}
 
@@ -186,18 +189,61 @@ def getSubjectData(subject_path, modalities, clas):
                 filter(lambda x: os.path.splitext(x)[0],
                 gathered_modalityPaths[modality])
             )
-    print(gathered_modalityPaths)
-
-
-    subject_data = {}
-    cropType = 'pixel'
     #crop type mapping: {'centerCrop'->'center','pixelCrop'->'pixel',None->'fullImage'}
+    cropTypeMapping = {
+        'centerCrop':'center',
+        'pixelCrop':'pixel',
+        'fullImage': None
+    }
     for modality,names in gathered_modalityPaths.items():
         subject_data[modality] = {
-            os.path.splitext(name)[0]:getImage(os.path.join(subject_path,modality,name),os.path.join(subject_path,modality+'L',name),cropType) for name in names
+            os.path.splitext(name)[0]:getImage(os.path.join(subject_path,modality,name),os.path.join(subject_path,modality+'L',name),cropTypeMapping[cropType]) for name in names
         }
 
-    print(subject_data)
+    #print(subject_data)
+    return subject_data
+
+def combineData(subjectPath, storePath, cropType):
+    # tempSubjectPath = '/home/maanvi/LAB/Datasets/kt_new_trainvaltest/am_pc_tm/train/CCRCC/17672172'
+    subject_data = getSubjectData(subjectPath, cropType)
+    print(subject_data['path'])
+    modalities = subject_data['path'].rsplit(os.path.sep,4)[1]
+    try:
+        if len(modalities) > 2:
+            assert '_' in modalities
+            modalities = modalities.split('_')
+        else:
+            modalities = [f'{modalities}']
+    except AssertionError:
+        print(f"need modailities with _ but got this instead '{modalities}'")
+
+    print(modalities)
+    imageNames = list(subject_data[modalities[0]].keys())
+    for name in imageNames:
+        image = None
+        for modality in modalities:
+            if image is None:
+                image = np.array(subject_data[modality][name])
+            else:
+                image = np.dstack([image, np.array(subject_data[modality][name])])
+        
+        
+        if len(modalities) == 1:
+            #one modality, duplicate thrice
+            image = np.repeat(image[:,:,np.newaxis],3,axis=-1)
+        elif len(modalities) == 2:
+            #two modalities, add 0 array as third dimension
+            zeros = np.zeros((image.shape[0],image.shape[1],1))
+            # print(f'{zeros.shape} zeros shape')
+            # print(f'{image.shape} prev img shape')
+            image = np.dstack((image,zeros))
+        
+        print(f"{name}.png -- shape {image.shape}")
+        filename = f"{subject_data['ID']}_{subject_data['clas']}_{name}.npz"
+        np.savez(os.path.join(storePath,filename))
+        #cv2.imwrite(f'/home/maanvi/Desktop/combined{name}.png',image)
+    
+    return image
 
 
 #next tasks
