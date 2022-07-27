b57769dbbd8cd7a0ca2184f4bc937ad7f0ad646f
nunna-m <maanvi7062@gmail.com>
2022-07-27 18:10:34 +0900

    main function to create numpy data aggregate
    function where are utlity functions are called and data is aggregated.pass cmd arguments here

---
+++ ktc/aggregate.py
@@ -0,0 +1,21 @@
+import os
+import newCrossVal
+import newFolders
+
+datasets_root = '/home/maanvi/LAB/Datasets'
+oldPath = os.path.join(datasets_root,'kt_new_trainvaltest')
+newPath = os.path.join(datasets_root,'kt_combined')
+os.makedirs(newPath, exist_ok=True)
+
+
+def main():
+    #first create raw data folder in ktcombined
+    newFolders.createRawDataFolder(oldPath=oldPath, newPath=newPath)
+    for modFolder in os.listdir(oldPath):
+        path = os.path.join(newPath,modFolder,'rawData')
+        newCrossVal.createFolds(basePath=path)
+    newFolders.createNumpyFiles(oldPath=oldPath, newPath=newPath)
+
+
+main()
+    
