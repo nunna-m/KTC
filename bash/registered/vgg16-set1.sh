2425d48d68ebd5fbc1b3c1054a2a684234527081
nunna-m <maanvi7062@gmail.com>
2022-08-28 15:04:37 +0900

    Update bash files to run remotely.

--- bash/registered/vgg16-set1.sh
+++ bash/registered/vgg16-set1.sh
@@ -1,8 +1,8 @@
-python3 -m ktc train --whichos remote --modalities am --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
-python3 -m ktc train --whichos remote --modalities tm --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
-python3 -m ktc train --whichos remote --modalities dc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
-python3 -m ktc train --whichos remote --modalities ec --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
-python3 -m ktc train --whichos remote --modalities pc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
-python3 -m ktc train --whichos remote --modalities dc ec --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
-python3 -m ktc train --whichos remote --modalities ec pc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
-python3 -m ktc train --whichos remote --modalities dc pc --method both --network vgg16 --config "configfiles/extra/data_options.yaml" --max_steps 50 --filename vgg16_exactTumor
+python3 -m ktc train_registered --whichos remote --modalities am --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
+python3 -m ktc train_registered --whichos remote --modalities tm --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
+python3 -m ktc train_registered --whichos remote --modalities dc --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
+python3 -m ktc train_registered --whichos remote --modalities ec --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
+python3 -m ktc train_registered --whichos remote --modalities pc --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
+python3 -m ktc train_registered --whichos remote --modalities dc ec --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
+python3 -m ktc train_registered --whichos remote --modalities ec pc --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
+python3 -m ktc train_registered --whichos remote --modalities dc pc --method both --network vgg16 --config "configfiles/extra/data_options_registered.yaml" --max_steps 50 --filename vgg16_boxCrop
