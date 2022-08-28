fc5db16dd5ab0fb274fcb30756adca0f6b59d393
nunna-m <maanvi7062@gmail.com>
2022-08-28 14:43:27 +0900

    Include train registered file in parser.

--- ktc/app.py
+++ ktc/app.py
@@ -4,7 +4,7 @@ import argparse
 
 import dsargparse
 
-from ktc.cl import train
+from ktc.cl import train, train_registered
 from ktc.cl import evaluate
 from ktc.cl import split_train
 from ktc.cl import train_stacked
@@ -16,6 +16,7 @@ def run(prog='python3 -m ktc'):
     subparsers.add_parser(split_train.split_train, add_arguments_auto=True)
     subparsers.add_parser(train_stacked.train_stacked, add_arguments_auto=True)
     subparsers.add_parser(train_stacked.meta_learner, add_arguments_auto=True)
+    subparsers.add_parser(train_registered.train_registered, add_arguments_auto=True)
     subparsers.add_parser(evaluate.evaluate, add_arguments_auto=True)
     return parser.parse_and_run()
 
