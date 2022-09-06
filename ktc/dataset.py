eba26ebaa9354965b70435eb94b4f4ce74dba94d
nunna-m <maanvi7062@gmail.com>
2022-09-06 17:44:50 +0900

    update

--- ktc/dataset.py
+++ ktc/dataset.py
@@ -741,48 +741,50 @@ def get_tumor_boundingbox_registered(imgpath, labelpath):
     then calculate bounding box around it after zooming out by a factor of 0.3 on both heigth and width (just to be sure of including the entire region of the tumor)
     am modality is gaussian standardized also
     '''
-    orig_image = cv2.imread(imgpath)[:,:,0]
-    #cv2.imwrite(f'/home/maanvi/registered_{imgpath[-5]}.png',orig_image)
-    (orig_height, orig_width) = cv2.imread(imgpath)[:,:,0].shape
-    mask = cv2.imread(labelpath)[:,:,0]
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2. CHAIN_APPROX_NONE)
-    c = max(contours, key=cv2.contourArea)
-    x, y, w, h = cv2.boundingRect(c)
-    assert (w,h)<(224,224)
-    assert (x,y)>=(0,0)
-    const = 0.3
-    diff_x = int(const*w)
-    diff_y = int(const*h)
-    if (x-diff_x)<0:
-        x1 = 0
-    else:
-        x1 = x-diff_x
-    if (y-diff_y)<0:
-        y1 = 0
-    else:
-        y1 = y-diff_y
-    if (x+w+diff_x)>=orig_width:
-        x2 = orig_width
-    else:
-        x2 = x+diff_x+w
-    if (y+diff_y+h)>=orig_height:
-        y2 = orig_height
-    else:
-        y2 = y+diff_y+h
+    # orig_image = cv2.imread(imgpath)[:,:,0]
+    # #cv2.imwrite(f'/home/maanvi/registered_{imgpath[-5]}.png',orig_image)
+    # (orig_height, orig_width) = cv2.imread(imgpath)[:,:,0].shape
+    # mask = cv2.imread(labelpath)[:,:,0]
+    # contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2. CHAIN_APPROX_NONE)
+    # c = max(contours, key=cv2.contourArea)
+    # x, y, w, h = cv2.boundingRect(c)
+    # assert (w,h)<(224,224)
+    # assert (x,y)>=(0,0)
+    # const = 0.3
+    # diff_x = int(const*w)
+    # diff_y = int(const*h)
+    # if (x-diff_x)<0:
+    #     x1 = 0
+    # else:
+    #     x1 = x-diff_x
+    # if (y-diff_y)<0:
+    #     y1 = 0
+    # else:
+    #     y1 = y-diff_y
+    # if (x+w+diff_x)>=orig_width:
+    #     x2 = orig_width
+    # else:
+    #     x2 = x+diff_x+w
+    # if (y+diff_y+h)>=orig_height:
+    #     y2 = orig_height
+    # else:
+    #     y2 = y+diff_y+h
 
 
-    #gaussian standardizes only modality am
-    tmp = imgpath.rsplit(os.path.sep,2)[1]
-    if tmp=='am':
-        mean, std = orig_image.mean(), orig_image.std()
-        orig_image = (orig_image - mean)/std
-        mean, std = orig_image.mean(), orig_image.std()
-        orig_image = np.clip(orig_image, -1.0, 1.0)
-        orig_image = (orig_image + 1.0) / 2.0
-        orig_image *= 255
-    backup = orig_image[y1:y2,x1:x2]
-    backup = cv2.resize(backup, (224,224),interpolation = cv2.INTER_LINEAR)
-    #cv2.imwrite(f'/home/maanvi/registered_resize{imgpath[-5]}.png',backup)
+    # #gaussian standardizes only modality am
+    # tmp = imgpath.rsplit(os.path.sep,2)[1]
+    # if tmp=='am':
+    #     mean, std = orig_image.mean(), orig_image.std()
+    #     orig_image = (orig_image - mean)/std
+    #     mean, std = orig_image.mean(), orig_image.std()
+    #     orig_image = np.clip(orig_image, -1.0, 1.0)
+    #     orig_image = (orig_image + 1.0) / 2.0
+    #     orig_image *= 255
+    # backup = orig_image[y1:y2,x1:x2]
+    # backup = cv2.resize(backup, (224,224),interpolation = cv2.INTER_LINEAR)
+    # #cv2.imwrite(f'/home/maanvi/registered_resize{imgpath[-5]}.png',backup)
+    new_path = imgpath.replace('kt_registered','kt_registered_box')
+    backup = cv2.imread(new_path)[:,:,0]
     backup = tf.convert_to_tensor(backup, dtype=tf.uint8)
     #print(backup.shape)
     return backup
