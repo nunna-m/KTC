40fd0c2ef754d3efef10293edb4ea6a04df3253a
nunna-m <maanvi7062@gmail.com>
2023-01-12 15:24:47 +0900

    Train tensorflow vgg16

--- ktc/trainMedMNIST.py
+++ ktc/trainMedMNIST.py
@@ -2,31 +2,103 @@ import cv2
 import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
 from tensorflow.keras.layers import Dense,Flatten,Conv2D,Activation,Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import EarlyStopping,ModelCheckpoint
 from tensorflow.keras.layers import MaxPool2D
 
+def VGG16(num_classes):
+    model = Sequential()
+    model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2),name='vgg16'))
+    model.add(Flatten(name='flatten'))
+    model.add(Dense(256, activation='relu', name='fc1'))
+    model.add(Dense(128, activation='relu', name='fc2'))
+    model.add(Dense(num_classes, activation='sigmoid', name='output'))
+    return model
 data = np.load('/home/maanvi/LAB/code/organamnist.npz', allow_pickle=True)
-print(list(data.keys()))
+#print(list(data.keys()))
 train_data = data['train_images']
-print(f'Train data shape: {train_data.shape}')
+train_labels = data['train_labels']
+#print(f'Train data shape: {train_data.shape}')
 val_data = data['val_images']
-print(f'Val data shape: {val_data.shape}')
+val_labels = data['val_labels']
+#print(f'Val data shape: {val_data.shape}')
 test_data = data['test_images']
-print(f'Test data shape: {test_data.shape}')
-
+test_labels = data['test_labels']
+#print(f'Test data shape: {test_data.shape}')
 class_labels = list(np.unique(data['train_labels']))
-train_datagen = ImageDataGenerator(
-    zoom_range=0.15,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.15,
-)
-test_datagen = ImageDataGenerator()
 
-train_generator = train_datagen.flow_from_dataframe
+def format_example(image, label):
+    image = tf.cast(image, tf.float32)
+    image = tf.expand_dims(image, axis=-1)
+    image = tf.image.grayscale_to_rgb(image)
+    image = tf.image.resize(image, (224,224))
+    return image, label
+
+normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./127.5, offset=-1)
+
+train_dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
+train_dataset = train_dataset.map(format_example)
+train_dataset = train_dataset.map(lambda x,y:(normalization_layer(x),y))
+
+val_dataset = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
+val_dataset = val_dataset.map(format_example)
+val_dataset = val_dataset.map(lambda x,y:(normalization_layer(x),y))
+
+test_dataset = tf.data.Dataset.from_tensor_slices((test_data, test_labels))
+test_dataset = test_dataset.map(format_example)
+test_dataset = test_dataset.map(lambda x,y:(normalization_layer(x),y))
+
+BATCH_SIZE = 32
+SHUFFLE_BUFFER_SIZE = 100
+train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+val_dataset = val_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+for image_batch, label_batch in train_dataset.take(2):
+    print(image_batch.shape)
+    print(label_batch.shape)
+    break
+
+# model = tf.keras.Sequential([
+#   tf.keras.layers.Conv2D(32, 3, activation='relu',input_shape=(224,224,3)),
+#   tf.keras.layers.MaxPooling2D(),
+#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#   tf.keras.layers.MaxPooling2D(),
+#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#   tf.keras.layers.MaxPooling2D(),
+#   tf.keras.layers.Flatten(),
+#   tf.keras.layers.Dense(128, activation='relu'),
+#   tf.keras.layers.Dense(len(class_labels))
+# ])
+model = VGG16(num_classes=len(class_labels))
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['sparse_categorical_accuracy'])
+
+model.fit(train_dataset, 
+          validation_data=val_dataset,
+          epochs=10)
