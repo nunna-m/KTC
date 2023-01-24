b5133d8cbae5c0bc2bab201bb2606f58cdefdc4d
nunna-m <maanvi7062@gmail.com>
2023-01-24 10:14:26 +0900

    alexnet and resnet box bash files

---
+++ bash/alexnet-box/set1.sh
@@ -0,0 +1,8 @@
+python3 -m ktc train --whichos remote --modalities am --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
+python3 -m ktc train --whichos remote --modalities tm --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
+python3 -m ktc train --whichos remote --modalities dc --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
+python3 -m ktc train --whichos remote --modalities ec --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
+python3 -m ktc train --whichos remote --modalities pc --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
+python3 -m ktc train --whichos remote --modalities dc ec --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
+python3 -m ktc train --whichos remote --modalities ec pc --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
+python3 -m ktc train --whichos remote --modalities dc pc --method both --network alexnet --config "configfiles/extra/data_options_box.yaml" --max_steps 50 --filename alexnet-box-am
